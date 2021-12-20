830ea1c8bf4246936bae3cca9ec021b70bd1d3bf
riyanmadeza <rmadeza@gmail.com>
2021-12-20 13:28:14 +0700

    Add Automatic Update

---
+++ FlashCalculation/FlashCalculationUpdate/FrmUpdate.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FlashCalculationUpdate
+{
+    public partial class FrmUpdate : Form
+    {
+        public FrmUpdate(List<MD5Update.FileUpdate> lisUpdates, string strUrlUpdateFolder)
+        {
+            try
+            {
+                InitializeComponent();
+                Download(lisUpdates, strUrlUpdateFolder);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        //Download
+        #region
+        public void Download(List<MD5Update.FileUpdate> lisUpdates, string strUrlUpdateFolder, bool bolAct = false)
+        {
+            try
+            {
+                bool bolUpdateApp = false;
+                int intFiles = 0;
+                long lonDownload = lisUpdates.Sum(v => v.LonSiz);
+                //long lonDownloaded = 0;
+                lblFiles.Text = intFiles + "/" + lisUpdates.Count + " Files downloaded";
+                foreach (MD5Update.FileUpdate Fil in lisUpdates)
+                {
+                    WebClient wcDownload = new WebClient();
+                    Uri urAct = new Uri(strUrlUpdateFolder + Fil.StrFil);
+                    string strFile = AppDomain.CurrentDomain.BaseDirectory + Fil.StrFil;
+                    string strDirectory = Path.GetDirectoryName(strFile);
+                    if (AppDomain.CurrentDomain.FriendlyName == Fil.StrFil)
+                    {
+                        bolUpdateApp = true;
+                        strDirectory += @"\updt\";
+                        strFile = strDirectory + Fil.StrFil;
+                    }
+                    if (!Directory.Exists(strDirectory))
+                    {
+                        Directory.CreateDirectory(strDirectory);
+                    }
+                    if (File.Exists(strFile))
+                    {
+                        File.SetAttributes(strFile, FileAttributes.Normal);
+                        File.Delete(strFile);
+                    }
+                    wcDownload.DownloadProgressChanged += new DownloadProgressChangedEventHandler(delegate (object sender, DownloadProgressChangedEventArgs e)
+                    {
+                        pbUpdate.Value = e.ProgressPercentage;
+                        /*pbUpdate.Value = (((int) lonDownloaded + e.ProgressPercentage) / (int)lonDownload) * 100;
+                        if(e.TotalBytesToReceive == e.BytesReceived)
+                        {
+                            lonDownloaded += e.TotalBytesToReceive;
+                        }*/
+                    });
+                    wcDownload.DownloadFileCompleted += new AsyncCompletedEventHandler(delegate (object sender, AsyncCompletedEventArgs e)
+                    {
+                        if (e.Error == null && !e.Cancelled)
+                        {
+                            intFiles++;
+                            lblFiles.Text = intFiles + "/" + lisUpdates.Count + " Files downloaded";
+                            if (intFiles == lisUpdates.Count)
+                            {
+                                if (bolUpdateApp)
+                                {
+                                    DialogResult = DialogResult.OK;
+                                }
+                                if (File.Exists(AppDomain.CurrentDomain.BaseDirectory + "updt.json"))
+                                {
+                                    File.Delete(AppDomain.CurrentDomain.BaseDirectory + "updt.json");
+                                }
+                                Close();
+                            }
+                        }
+                    });
+                    wcDownload.DownloadFileAsync(urAct, strFile);
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        #endregion
+    }
+}
---
+++ FlashCalculation/FlashCalculationUpdate/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FlashCalculationUpdate
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new FrmUpdate());
+
+            //Process Update
+            //string strUrl = "http://update.ucmasidn.com/";
+            if (MD5Update.Check(Properties.Settings.Default.url, true))
+            {
+                Process.Start(AppDomain.CurrentDomain.BaseDirectory + @"updt.exe", AppDomain.CurrentDomain.FriendlyName + " " + Process.GetCurrentProcess().ProcessName);
+            }
+            else
+            {
+                Application.Exit();
+            }
+
+            Process.Start("FlashCalculation.exe");
+            Application.Exit();
+        }
+    }
+}
